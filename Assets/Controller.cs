a2eb96f8a6076bc2ef03b5f68fe3080337109db7
AleksandrDavletshin <77685325+AleksandrDavletshin@users.noreply.github.com>
2021-03-16 18:58:49 +0300

    Update 0.2
    Add pause button

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -8,6 +8,7 @@ public class Controller : MonoBehaviour
     public UnityEngine.UI.Text scoreLable;
     public UnityEngine.UI.Image menu;
     public UnityEngine.UI.Button startButton;
+    public UnityEngine.UI.Button pauseButton;
     public static int score = 0;
     public static bool isStarted = false;//запущена ли игра
     // Start is called before the first frame update
@@ -18,12 +19,19 @@ public class Controller : MonoBehaviour
             menu.gameObject.SetActive(false);
             isStarted = true;//запуск игры
         });
-        
+
+        pauseButton.onClick.AddListener(TogglePause);
+    }
+
+    public void TogglePause()
+    {
+        Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         scoreLable.text = "Score: " + score;
     }
 }
